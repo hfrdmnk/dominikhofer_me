b6485300bc424e94ecd5b7f6b8bcd32459b9b390
Dominik Hofer <hi@dominikhofer.me>
2024-05-20 11:42:13 +0200

    Added brand color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,7 +9,23 @@ export default {
 			serif: ['"Playfair Display"', ...defaultTheme.fontFamily.serif],
 			mono: ['"Space Grotesk"', ...defaultTheme.fontFamily.mono],
 		},
-		extend: {},
+		extend: {
+			colors: {
+				brand: {
+					50: '#fdffe4',
+					100: '#f8ffc4',
+					200: '#f0ff90',
+					300: '#e0ff50',
+					400: '#c9ff00',
+					500: '#afe600',
+					600: '#88b800',
+					700: '#668b00',
+					800: '#516d07',
+					900: '#455c0b',
+					950: '#233400',
+				},
+			},
+		},
 	},
 	plugins: [],
 };
