76b36c0c8776471b1e10ce3bc70653e2a1086542
Dominik Hofer <hi@dominikhofer.me>
2024-05-22 07:40:48 +0200

    Added fancy image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
-import tailwind from "@astrojs/tailwind";
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+	site: 'https://example.com',
+	// TODO: Configure only icons in use: https://www.astroicon.dev/reference/configuration#include
+	integrations: [mdx(), sitemap(), tailwind(), icon()],
 });
