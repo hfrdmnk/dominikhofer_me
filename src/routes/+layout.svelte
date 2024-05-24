c40edc0b24398bcd6b677100076492663dbde591
Dominik Hofer <hi@dominikhofer.me>
2024-05-24 22:18:49 +0200

    Added plausible

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,6 +12,11 @@
 	<title>dominikhofer dot me</title>
 	<meta name="description" content="My always-in-progress home on the internet." />
 	<meta property="og:image" content="https://dominikhofer.me/og-image.png" />
+	<script
+		defer
+		data-domain="dominikhofer.me"
+		src="https://analytics.linea.studio:8000/js/script.js"
+	></script>
 </svelte:head>
 
 <slot />
