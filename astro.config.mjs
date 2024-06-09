62d6f90cc04e0a7b3c5d28ad99f2f159364d40a2
Dominik Hofer <hi@dominikhofer.me>
2024-06-09 11:24:53 +0200

    Added react

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,15 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
-
 import vercel from '@astrojs/vercel/serverless';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	// TODO: Configure only icons in use: https://www.astroicon.dev/reference/configuration#include
-	integrations: [mdx(), sitemap(), tailwind(), icon()],
-	output: 'hybrid',
-	adapter: vercel(),
+  site: 'https://example.com',
+  // TODO: Configure only icons in use: https://www.astroicon.dev/reference/configuration#include
+  integrations: [mdx(), sitemap(), tailwind(), icon(), react()],
+  output: 'hybrid',
+  adapter: vercel()
 });
