09543c8c4cfd44ff75474cbbd0347b7f65268792
Dominik Hofer <hi@dominikhofer.me>
2024-06-09 12:15:38 +0200

    Animated fancy image

--- src/components/FancyImage.tsx
+++ src/components/FancyImage.tsx
@@ -1,7 +1,22 @@
+import { useState, useRef } from "react";
+import { motion, useDomEvent } from "framer-motion";
+
 import { MapPin } from "@phosphor-icons/react";
 import { formatDistanceToNow } from "date-fns";
+import { default as cn } from "classnames";
 
-const PhotoCard = ({
+const openTransition = {
+  type: "spring",
+  damping: 20,
+  stiffness: 250,
+};
+
+const closeTransition = {
+  type: "tween",
+  duration: 0,
+};
+
+const FancyImage = ({
   className,
   image,
   alt,
@@ -16,6 +31,12 @@ const PhotoCard = ({
   displayName: string;
   date?: string;
 }) => {
+  const [isOpen, setOpen] = useState(false);
+
+  if (typeof window !== "undefined") {
+    useDomEvent(useRef(window), "scroll", () => isOpen && setOpen(false));
+  }
+
   let displayDate;
 
   if (date) {
@@ -24,15 +45,34 @@ const PhotoCard = ({
 
   return (
     <figure className="select-none space-y-2">
-      <div className="relative overflow-hidden rounded">
-        <img src={image} alt={alt} className={`${className} object-cover`} />
-        <div className="absolute bottom-0 left-0 right-0 z-20 flex items-center justify-end px-4 py-6 font-mono text-sm text-gray-300">
+      <div
+        className={cn(
+          "image-container relative overflow-hidden rounded",
+          className,
+          isOpen ? "open" : "",
+        )}
+      >
+        <motion.div
+          animate={{ opacity: isOpen ? 1 : 0 }}
+          transition={isOpen ? openTransition : closeTransition}
+          className="shade"
+          onClick={() => setOpen(false)}
+        />
+        <motion.img
+          src={image}
+          alt={alt}
+          className="absolute inset-0 h-full w-full rounded object-cover"
+          onClick={() => setOpen(!isOpen)}
+          layout
+          transition={isOpen ? openTransition : closeTransition}
+        />
+        <div className="meta pointer-events-none absolute bottom-0 left-0 right-0 z-20 flex items-center justify-end px-4 py-6 font-mono text-sm text-gray-300">
           <div className="flex items-center gap-2">
-            <MapPin name="ph:map-pin" className="h-5 w-5 text-brand-400" />
+            <MapPin className="h-5 w-5 text-brand-400" />
             <span>{location}</span>
           </div>
         </div>
-        <div className="absolute bottom-0 left-0 right-0 top-2/3 z-10 bg-gradient-to-b from-transparent to-black"></div>
+        <div className="meta-gradient pointer-events-none absolute bottom-0 left-0 right-0 top-2/3 z-10 bg-gradient-to-b from-transparent to-black"></div>
       </div>
       <figcaption className="flex justify-between font-mono text-sm">
         <span>{date && `${displayDate} ago`}</span>
@@ -42,4 +82,4 @@ const PhotoCard = ({
   );
 };
 
-export default PhotoCard;
+export default FancyImage;
