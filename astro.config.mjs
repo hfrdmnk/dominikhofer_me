5c65c911eef61ba51bded88ec9407440e67f95f0
Dominik Hofer <hi@dominikhofer.me>
2024-06-12 09:19:47 +0200

    Added astro compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,15 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import vercel from '@astrojs/vercel/serverless';
-
 import react from "@astrojs/react";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   // TODO: Configure only icons in use: https://www.astroicon.dev/reference/configuration#include
-  integrations: [mdx(), sitemap(), tailwind(), icon(), react()],
+  integrations: [mdx(), sitemap(), tailwind(), icon(), react(), compress()],
   output: 'hybrid',
   adapter: vercel()
 });
