010be41387f3a61db0ac42293653c4102172b120
Dominik Hofer <hi@dominikhofer.me>
2024-06-12 09:33:53 +0200

    Added suspense package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,27 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import icon from 'astro-icon';
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
-
 import compress from "astro-compress";
+
+import simpleStackStream from "simple-stack-stream";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: "https://example.com",
   // TODO: Configure only icons in use: https://www.astroicon.dev/reference/configuration#include
-  integrations: [mdx(), sitemap(), tailwind(), icon(), react(), compress()],
-  output: 'hybrid',
-  adapter: vercel()
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    icon(),
+    react(),
+    simpleStackStream(),
+    compress(),
+  ],
+  output: "hybrid",
+  adapter: vercel(),
 });
