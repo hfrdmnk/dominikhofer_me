3c143aa2d16c8baaedec419a721324d0f109bd40
Dominik Hofer <hi@dominikhofer.me>
2024-06-12 10:21:54 +0200

    Cleanup (react & other image stuff)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,24 +4,13 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import vercel from "@astrojs/vercel/serverless";
-import react from "@astrojs/react";
 import compress from "astro-compress";
-
-import simpleStackStream from "simple-stack-stream";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   // TODO: Configure only icons in use: https://www.astroicon.dev/reference/configuration#include
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind(),
-    icon(),
-    react(),
-    simpleStackStream(),
-    compress(),
-  ],
+  integrations: [mdx(), sitemap(), tailwind(), icon(), compress()],
   output: "hybrid",
   adapter: vercel(),
 });
