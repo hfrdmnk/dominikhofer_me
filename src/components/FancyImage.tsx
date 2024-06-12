3821e46b2c8d640d22806826fa16dd62a89a5ab4
Dominik Hofer <hi@dominikhofer.me>
2024-06-12 09:14:10 +0200

    Tried to lazy load images

--- src/components/FancyImage.tsx
+++ src/components/FancyImage.tsx
@@ -1,5 +1,6 @@
 import { useState, useRef } from "react";
 import { motion, useDomEvent } from "framer-motion";
+import LazyLoad from "react-lazy-load";
 
 import { MapPin } from "@phosphor-icons/react";
 import { formatDistanceToNow } from "date-fns";
@@ -46,41 +47,45 @@ const FancyImage = ({
   }
 
   return (
-    <figure className="select-none space-y-2">
-      <div
-        className={cn(
-          "image-container relative overflow-hidden rounded",
-          className,
-          isOpen ? "open" : "",
-        )}
-      >
-        <motion.div
-          animate={{ opacity: isOpen ? 1 : 0 }}
-          transition={isOpen ? openTransition : closeTransition}
-          className="shade"
-          onClick={() => setOpen(false)}
-        />
-        <motion.img
-          src={image}
-          alt={alt}
-          className="absolute inset-0 h-full w-full rounded object-cover"
-          onClick={() => setOpen(!isOpen)}
-          layout
-          transition={isOpen ? openTransition : closeTransition}
-        />
-        <div className="meta pointer-events-none absolute bottom-0 left-0 right-0 z-20 flex items-center justify-end px-4 py-6 font-mono text-sm text-gray-300">
-          <div className="flex items-center gap-2">
-            <MapPin className="h-5 w-5 text-brand-400" />
-            <span>{location}</span>
+    <LazyLoad>
+      <figure className="select-none space-y-2">
+        <div
+          className={cn(
+            "image-container relative overflow-hidden rounded",
+            className,
+            isOpen ? "open" : "",
+          )}
+        >
+          <motion.div
+            animate={{ opacity: isOpen ? 1 : 0 }}
+            transition={isOpen ? openTransition : closeTransition}
+            className="shade"
+            onClick={() => setOpen(false)}
+          />
+          <motion.img
+            width={250}
+            height={250}
+            src={image}
+            alt={alt}
+            className="absolute inset-0 h-full w-full rounded object-cover"
+            onClick={() => setOpen(!isOpen)}
+            layout
+            transition={isOpen ? openTransition : closeTransition}
+          />
+          <div className="meta pointer-events-none absolute bottom-0 left-0 right-0 z-20 flex items-center justify-end px-4 py-6 font-mono text-sm text-gray-300">
+            <div className="flex items-center gap-2">
+              <MapPin className="h-5 w-5 text-brand-400" />
+              <span>{location}</span>
+            </div>
           </div>
+          <div className="meta-gradient pointer-events-none absolute bottom-0 left-0 right-0 top-2/3 z-10 bg-gradient-to-b from-transparent to-black"></div>
         </div>
-        <div className="meta-gradient pointer-events-none absolute bottom-0 left-0 right-0 top-2/3 z-10 bg-gradient-to-b from-transparent to-black"></div>
-      </div>
-      <figcaption className="flex justify-between font-mono text-sm">
-        <span>{date && `${displayDate} ago`}</span>
-        <span>{displayName}</span>
-      </figcaption>
-    </figure>
+        <figcaption className="flex justify-between font-mono text-sm">
+          <span>{date && `${displayDate} ago`}</span>
+          <span>{displayName}</span>
+        </figcaption>
+      </figure>
+    </LazyLoad>
   );
 };
 
