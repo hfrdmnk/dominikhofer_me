6d84dd4662fd309fac0b6fe37cb977e45f25b92b
Dominik Hofer <hi@dominikhofer.me>
2024-08-16 17:15:39 +0200

    Testing images in rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,9 +1,16 @@
 import rss from "@astrojs/rss";
+import { getImage } from "astro:assets";
 import { getCollection } from "astro:content";
 import MarkdownIt from "markdown-it";
+import footnotePlugin from "markdown-it-footnote";
 import replaceLinkPlugin from "markdown-it-replace-link";
+import htmlParser from "node-html-parser";
 import sanitizeHtml from "sanitize-html";
 import { SITE_AUTHOR, SITE_DESCRIPTION, SITE_TITLE } from "../consts";
+
+const imagesGlob = import.meta.glob(
+  "/src/assets/posts/**/*.{jpeg,jpg,png,gif}",
+);
 
 export async function GET(context) {
   // Setup Link Parser
@@ -16,6 +23,7 @@ export async function GET(context) {
       return link;
     },
   });
+  parser.use(footnotePlugin);
 
   // Get all posts and sort them by date
   const posts = await getCollection("posts");
@@ -23,7 +31,74 @@ export async function GET(context) {
     return new Date(b.data.date) - new Date(a.data.date);
   });
 
-  console.log(sortedPosts[0].data.visual);
+  // Create the feed
+  const feed = [];
+
+  // Build the feed
+  for (const post of sortedPosts) {
+    // Setup footer
+    const footerContent = `
+      <footer>
+        <p><strong>---</strong></p>
+        <p><strong>Thanks for being an RSS subscriber!</strong></p>
+        <p>Reply to this post via <a href="mailto:hi@dominikhofer.me?subject=${encodeURIComponent(`Reply to: ${post.data.title}`)}">email</a>.</p>
+      </footer>`;
+
+    // Modify the post body
+    const body = parser.render(post.body + footerContent);
+    // convert html string to DOM-like structure
+    const html = htmlParser.parse(body);
+    // hold all img tags in variable images
+    const images = html.querySelectorAll("img");
+
+    for (const img of images) {
+      const src = img.getAttribute("src");
+
+      // Relative paths that are optimized by Astro build
+      if (src.startsWith("../../")) {
+        // remove prefix of `../../`
+        const prefixRemoved = src.replace("../../", "");
+        // create prefix absolute path from root dir
+        const imagePathPrefix = `/src/${prefixRemoved}`;
+
+        // call the dynamic import and return the module
+        const imagePath = await imagesGlob[imagePathPrefix]?.()?.then(
+          (res) => res.default,
+        );
+
+        if (imagePath) {
+          const optimizedImg = await getImage({ src: imagePath });
+          // set the correct path to the optimized image
+          img.setAttribute(
+            "src",
+            context.site + optimizedImg.src.replace("/", ""),
+          );
+        }
+      } else {
+        throw Error("src unknown");
+      }
+    }
+
+    feed.push({
+      title: post.data.title,
+      pubDate: post.data.date,
+      description: post.data.excerpt,
+      author: SITE_AUTHOR,
+      link: `/${post.slug}`,
+      content: sanitizeHtml(html.toString(), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+      }),
+      customData: post.data.visual
+        ? `<media:content
+      type="image/${post.data.visual.format == "jpg" ? "jpeg" : "png"}"
+      width="${post.data.visual.width}"
+      height="${post.data.visual.height}"
+      medium="image"
+      url="${context.site.origin + post.data.visual.src}" />
+  `
+        : undefined,
+    });
+  }
 
   return rss({
     title: `All Posts | ${SITE_TITLE}`,
@@ -34,34 +109,8 @@ export async function GET(context) {
       media: "http://search.yahoo.com/mrss/",
       atom: "http://www.w3.org/2005/Atom",
     },
-    stylesheet: "/styles/rss.xsl",
+    // stylesheet: "/styles/rss.xsl",
     customData: `<atom:link href="${context.site}rss.xml" rel="self" type="application/rss+xml" />`,
-    items: sortedPosts.map((post) => {
-      const footerContent = `
-      <footer>
-        <p><strong>---</strong></p>
-        <p><strong>Thanks for being an RSS subscriber!</strong></p>
-        <p>Reply to this post via <a href="mailto:hi@dominikhofer.me?subject=${encodeURIComponent(`Reply to: ${post.data.title}`)}">email</a>.</p>
-      </footer>`;
-      return {
-        title: post.data.title,
-        pubDate: post.data.date,
-        description: post.data.excerpt,
-        author: SITE_AUTHOR,
-        link: `/${post.slug}`,
-        content: sanitizeHtml(parser.render(post.body) + footerContent, {
-          allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
-        }),
-        customData: post.data.visual
-          ? `<media:content
-      type="image/${post.data.visual.format == "jpg" ? "jpeg" : "png"}"
-      width="${post.data.visual.width}"
-      height="${post.data.visual.height}"
-      medium="image"
-      url="${context.site.origin + post.data.visual.src}" />
-  `
-          : undefined,
-      };
-    }),
+    items: feed,
   });
 }
