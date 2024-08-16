6d4c0167dbb4ef9182e72b88d4fa9f40592f5ec1
Dominik Hofer <hi@dominikhofer.me>
2024-08-16 16:06:13 +0200

    Testing new rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,30 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
-import sanitizeHtml from "sanitize-html";
 import MarkdownIt from "markdown-it";
-const parser = new MarkdownIt();
+import replaceLinkPlugin from "markdown-it-replace-link";
+import sanitizeHtml from "sanitize-html";
+import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
 
 export async function GET(context) {
+  // Setup Link Parser
+  const parser = new MarkdownIt();
+  parser.use(replaceLinkPlugin, {
+    replaceLink: (link) => {
+      if (link.startsWith("/")) {
+        return context.site.origin + link;
+      }
+      return link;
+    },
+  });
+
+  // Get all posts and sort them by date
   const posts = await getCollection("posts");
+  const sortedPosts = posts.sort((a, b) => {
+    return new Date(b.data.date) - new Date(a.data.date);
+  });
+
+  console.log(sortedPosts[0].data.visual);
+
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
@@ -16,11 +34,13 @@ export async function GET(context) {
       media: "http://search.yahoo.com/mrss/",
       atom: "http://www.w3.org/2005/Atom",
     },
+    // stylesheet: "/styles/rss-styles.xsl",
     customData: `<atom:link href="${context.site}rss.xml" rel="self" type="application/rss+xml" />`,
-    items: posts.map((post) => ({
+    items: sortedPosts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.date,
       description: post.data.excerpt,
+      author: "Dominik Hofer",
       link: `/${post.slug}`,
       content: sanitizeHtml(parser.render(post.body), {
         allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
@@ -31,7 +51,7 @@ export async function GET(context) {
       width="${post.data.visual.width}"
       height="${post.data.visual.height}"
       medium="image"
-      url="${context.site + post.data.visual.src.replace(/^(\.\.\/)*/, "/")}" />
+      url="${context.site.origin + post.data.visual.src}" />
   `
         : undefined,
     })),
