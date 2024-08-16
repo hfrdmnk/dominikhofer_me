8fc86f46f01ae9bb95fc9009a3bc065ae9d64c6c
Dominik Hofer <hi@dominikhofer.me>
2024-08-16 14:09:38 +0200

    Changed config to better work with DarkMatter

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,16 @@
 import { defineCollection, z } from "astro:content";
-import { dateTime } from "darkmatter";
+import { text } from "darkmatter";
 
 const posts = defineCollection({
   type: "content",
-  // Type-check frontmatter using a schema
   schema: ({ image }) =>
     z.object({
       title: z.string(),
-      excerpt: z.string().optional(),
-      type: z.enum(["post", "essay", "note"]).default("post"),
+      excerpt: text(),
+      type: z.enum(["post", "essay", "note"]).default("note"),
       tags: z.array(z.string()).default([]),
-      // Transform string to Date object
-      date: dateTime(),
-      lastUpdated: dateTime().optional(),
+      date: z.coerce.string(),
+      lastUpdated: z.coerce.string().optional(),
       visual: image().optional(),
       isFeatured: z.boolean().default(false),
     }),
@@ -20,11 +18,10 @@ const posts = defineCollection({
 
 const til = defineCollection({
   type: "content",
-  schema: ({ image }) =>
+  schema: () =>
     z.object({
       title: z.string(),
-      // Transform string to Date object
-      date: z.coerce.date(),
+      date: z.coerce.string(),
       source: z.string().url(),
     }),
 });
@@ -35,21 +32,19 @@ const photos = defineCollection({
     z.object({
       visual: image(),
       title: z.string(),
-      // Transform string to Date object
-      date: z.coerce.date().optional(),
-      location: z.string().optional(),
+      date: z.coerce.string(),
+      location: z.string(),
     }),
 });
 
 const races = defineCollection({
   type: "content",
-  schema: ({ image }) =>
+  schema: () =>
     z.object({
       title: z.string(),
-      // Transform string to Date object
-      date: z.coerce.date(),
-      location: z.string(),
-      distance: z.number(),
+      date: z.coerce.string(),
+      location: z.coerce.string(),
+      distance: z.coerce.string(),
       time: z.coerce.string(),
       pace: z.coerce.string(),
     }),
@@ -66,9 +61,8 @@ const milestones = defineCollection({
       company: z.string().optional(),
       link: z.string().url().optional(),
       isFeatured: z.boolean().default(false),
-      // Transform string to Date object
-      date: z.coerce.date(),
-      dateEnd: z.coerce.date().optional(),
+      date: z.coerce.string(),
+      dateEnd: z.coerce.string().optional(),
       visual: image().optional(),
     }),
 });
