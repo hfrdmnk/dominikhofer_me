6350176d99863913edd499598bb42ad3a2a744f1
Dominik Hofer <hi@dominikhofer.me>
2024-08-17 12:03:11 +0200

    Just a simple redirect now

--- src/pages/.well-known/webfinger.ts
+++ src/pages/.well-known/webfinger.ts
@@ -1,17 +1,8 @@
-// Adapted form https://www.seanmcp.com/articles/use-your-domain-on-mastodon-with-astro/
-
-export const prerender = false;
-
 const INSTANCE = "mastodon.design";
 const USERNAME = "dominik";
 
-export async function GET() {
-  const response = await fetch(
+export async function GET({ redirect }) {
+  return redirect(
     `https://${INSTANCE}/.well-known/webfinger?resource=acct:${USERNAME}@${INSTANCE}`,
   );
-  const data = await response.json();
-
-  return new Response(JSON.stringify(data, null, 2), {
-    headers: { "Content-Type": "application/json" },
-  });
 }
