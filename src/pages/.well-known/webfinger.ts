f199801b02a8340693addd75b7d220aec02df3fb
Dominik Hofer <hi@dominikhofer.me>
2024-08-17 12:07:40 +0200

    Added status code

--- src/pages/.well-known/webfinger.ts
+++ src/pages/.well-known/webfinger.ts
@@ -4,5 +4,6 @@ const USERNAME = "dominik";
 export async function GET({ redirect }) {
   return redirect(
     `https://${INSTANCE}/.well-known/webfinger?resource=acct:${USERNAME}@${INSTANCE}`,
+    301,
   );
 }
