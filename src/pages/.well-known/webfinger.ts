5794da45290bae9195967c52dad23a951aac63fd
Dominik Hofer <hi@dominikhofer.me>
2024-08-17 17:47:52 +0200

    Back to old implementation

--- src/pages/.well-known/webfinger.ts
+++ src/pages/.well-known/webfinger.ts
@@ -1,62 +1,37 @@
-// Endpoint for WebFinger requests
-
-import { SITE_URL } from "../../consts";
-
-export const prerender = false;
-
-const ACCOUNTS = [
-  {
-    username: "toots",
-    json: {
-      subject: "acct:dominik@mastodon.design",
-      aliases: [
-        "https://mastodon.design/@dominik",
-        "https://mastodon.design/users/dominik",
-      ],
-      links: [
-        {
-          rel: "http://webfinger.net/rel/profile-page",
-          type: "text/html",
-          href: "https://mastodon.design/@dominik",
-        },
-        {
-          rel: "self",
-          type: "application/activity+json",
-          href: "https://mastodon.design/users/dominik",
-        },
-        {
-          rel: "http://ostatus.org/schema/1.0/subscribe",
-          template: "https://mastodon.design/authorize_interaction?uri={uri}",
-        },
-        {
-          rel: "http://webfinger.net/rel/avatar",
-          type: "image/jpeg",
-          href: "https://cdn.masto.host/mastodondesign/accounts/avatars/109/284/796/890/120/955/original/9fdec81896647dcb.jpg",
-        },
-      ],
+// Endpoint for WebFinger
+const WEBFINGER_JSON = {
+  subject: "acct:dominik@mastodon.design",
+  aliases: [
+    "https://mastodon.design/@dominik",
+    "https://mastodon.design/users/dominik",
+  ],
+  links: [
+    {
+      rel: "http://webfinger.net/rel/profile-page",
+      type: "text/html",
+      href: "https://mastodon.design/@dominik",
     },
-  },
-];
+    {
+      rel: "self",
+      type: "application/activity+json",
+      href: "https://mastodon.design/users/dominik",
+    },
+    {
+      rel: "http://ostatus.org/schema/1.0/subscribe",
+      template: "https://mastodon.design/authorize_interaction?uri={uri}",
+    },
+    {
+      rel: "http://webfinger.net/rel/avatar",
+      type: "image/jpeg",
+      href: "https://cdn.masto.host/mastodondesign/accounts/avatars/109/284/796/890/120/955/original/9fdec81896647dcb.jpg",
+    },
+  ],
+};
 
-const hostname = new URL(SITE_URL).hostname;
-
-export async function GET({ request }) {
-  const url = new URL(request.url);
-  const resource = url.searchParams.get("resource");
-
-  if (!resource) {
-    return new Response("Bad request", { status: 400 });
-  }
-
-  for (const account of ACCOUNTS) {
-    if (resource === `acct:${account.username}@${hostname}`) {
-      return new Response(JSON.stringify(account.json), {
-        headers: {
-          "Content-Type": "application/jrd+json",
-        },
-      });
-    }
-  }
-
-  return new Response("Not found", { status: 404 });
+export async function GET() {
+  return new Response(JSON.stringify(WEBFINGER_JSON), {
+    headers: {
+      "Content-Type": "application/activity+json",
+    },
+  });
 }
