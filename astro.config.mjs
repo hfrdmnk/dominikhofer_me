cc39bdd08dedb5111864b46da59cae54745dcb73
Dominik Hofer <hi@dominikhofer.me>
2024-08-23 09:56:55 +0200

    Publish "A song memory"

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import embeds from "astro-embed/integration";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
@@ -13,7 +14,7 @@ export default defineConfig({
   site: "https://dominikhofer.me",
   trailingSlash: "never",
   // TODO: Configure only icons in use: https://www.astroicon.dev/reference/configuration#include
-  integrations: [mdx(), tailwind(), alpinejs(), sitemap(), icon()],
+  integrations: [embeds(), mdx(), tailwind(), alpinejs(), sitemap(), icon()],
   output: "hybrid",
   adapter: vercel(),
   markdown: {
