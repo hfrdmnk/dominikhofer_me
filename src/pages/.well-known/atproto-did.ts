8587fcf7da111f4f282b820c8989c204730d81ea
Dominik Hofer <hi@dominikhofer.me>
2024-11-14 19:18:07 +0100

    New bluesky account

--- src/pages/.well-known/atproto-did.ts
+++ src/pages/.well-known/atproto-did.ts
@@ -1,5 +1,5 @@
 // Endpoint for AT Proto
-const ATPROTO_DID = "did:plc:lta2nlofz3f7tk4tpsg5vxfa";
+const ATPROTO_DID = "did:plc:fthx2gjakdj4ynxxu5vysjty";
 
 export async function GET() {
   return new Response(ATPROTO_DID, {
