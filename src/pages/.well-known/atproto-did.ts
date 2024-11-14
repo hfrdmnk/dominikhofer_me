175bf9f4dac09342f0ab1b0993c1caefc2e57928
Dominik Hofer <hi@dominikhofer.me>
2024-11-14 15:17:55 +0100

    Fixed potential bug

--- src/pages/.well-known/atproto-did.ts
+++ src/pages/.well-known/atproto-did.ts
@@ -2,7 +2,7 @@
 const ATPROTO_DID = "did:plc:lta2nlofz3f7tk4tpsg5vxfa";
 
 export async function GET() {
-  return new Response(JSON.stringify(ATPROTO_DID), {
+  return new Response(ATPROTO_DID, {
     headers: {
       "Content-Type": "text/plain",
     },
