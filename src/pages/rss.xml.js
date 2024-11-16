71d36a25f4f51d059ce067a293cf559b8e8054b6
Dominik Hofer <hi@dominikhofer.me>
2024-11-16 17:27:52 +0100

    Removed newsletter mentions

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -7,7 +7,6 @@ export async function GET(context) {
       <p><strong>Thanks for being an RSS subscriber!</strong></p>
       <p>Reply to this post via <a href="mailto:hi@dominikhofer.me?subject=${encodeURIComponent(`Reply to: ${title}`)}">email</a>.</p>
       ${webmentionsLink ? `<p>If you're on Mastodon, you can also reply to <a href="${webmentionsLink}">this post</a>.</p>` : ""}
-      <p>Or subscribe to my bi-weekly newsletter if you want to stay up date with everything I do: <a href="https://dominikhofer.me/newsletter">Tiny Sparks</a>.</p>
     </footer>`;
 
   return createRSSFeed(context, {
